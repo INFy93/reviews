03534987815adccf6a7829be5794af1ec583b3ce
INFy93 <infy93@gmail.com>
2021-02-11 09:20:02 +0300

    test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-Тест ответа: {{checkGigabyteUser('infy', 'r70399202')}}
+Тест ответа: {{checkGigabyteUser('infy', 'r703992023')}}
 
 @if ($responce->serverError())
     {{$responce->serverError()}}
