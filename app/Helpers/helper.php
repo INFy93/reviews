2a9371d7b92385945a5ce0cd237c217e0768c874
INFy93 <infy93@gmail.com>
2021-05-05 13:43:04 +0300

    Format code

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,81 +1,105 @@
 <?php
+
 namespace App\Helpers;
+
 use App\Models\Review;
 
-Class Features
+class Features
 {
-        public static function pureDate($date)
+    public static function pureDate($date)
+    {
+        $current_year = date('Y'); //текущий год
+        $review_year = date('Y', strtotime($date)); //год отзыва
+        $review_month = date('n', strtotime($date)); //месяц отзыва в числовом формате
+        $review_time = date('H:i', strtotime($date)); //время отзыва в формате 12:00
+
+        if ($current_year == $review_year) //если текущий год равен году отзыва, то...
         {
-            $current_year = date('Y'); //текущий год
-            $review_year = date('Y', strtotime($date)); //год отзыва
-            $review_month = date('n', strtotime($date)); //месяц отзыва в числовом формате
-            $review_time = date('H:i', strtotime($date)); //время отзыва в формате 12:00
-
-            if ($current_year == $review_year) //если текущий год равен году отзыва, то...
-            {
-                $year = ''; //не выводим его...
-            } else
-            {
-                $year = ' '.$review_year; //иначе = выводим
-            }
-
-            switch($review_month)
-            { //выводим корректно названия месяцев. можно было бы массивом, но мне лень.
-                case 1: $month = ' января'; break;
-                case 2: $month = ' февраля'; break;
-                case 3: $month = ' марта'; break;
-                case 4: $month = ' апреля'; break;
-                case 5: $month = ' мая'; break;
-                case 6: $month = ' июня'; break;
-                case 7: $month = ' июля'; break;
-                case 8: $month = ' августа'; break;
-                case 9: $month = ' сентября'; break;
-                case 10: $month = ' октября'; break;
-                case 11: $month = ' ноября'; break;
-                case 12: $month = ' декабря'; break;
-
-                default: $month = $review_month;
-            }
-
-            return date('j', strtotime($date)).$month.$year.' в '.$review_time; //выводим
-
+            $year = ''; //не выводим его...
+        } else {
+            $year = ' ' . $review_year; //иначе = выводим
         }
 
-        public static function getUserReviewCount($id) //получаем кол-во отзывов конкретного юзера для сводной таблицы
-        {
-            $user_review_conut = Review::where('user_id', '=', $id)->count();
+        switch ($review_month) { //выводим корректно названия месяцев. можно было бы массивом, но мне лень.
+            case 1:
+                $month = ' января';
+                break;
+            case 2:
+                $month = ' февраля';
+                break;
+            case 3:
+                $month = ' марта';
+                break;
+            case 4:
+                $month = ' апреля';
+                break;
+            case 5:
+                $month = ' мая';
+                break;
+            case 6:
+                $month = ' июня';
+                break;
+            case 7:
+                $month = ' июля';
+                break;
+            case 8:
+                $month = ' августа';
+                break;
+            case 9:
+                $month = ' сентября';
+                break;
+            case 10:
+                $month = ' октября';
+                break;
+            case 11:
+                $month = ' ноября';
+                break;
+            case 12:
+                $month = ' декабря';
+                break;
 
-            return $user_review_conut;
+            default:
+                $month = $review_month;
         }
 
+        return date('j', strtotime($date)) . $month . $year . ' в ' . $review_time; //выводим
 
-        public static function getAvgMark($id)
-        {
-            $query = Review::where('user_id', '=', $id)->get();
+    }
 
-            $avg_mark = round($query->avg('mark'), 2);
+    public static function getUserReviewCount($id) //получаем кол-во отзывов конкретного юзера для сводной таблицы
+    {
+        $user_review_conut = Review::where('user_id', '=', $id)->count();
 
-            return $avg_mark;
-        }
+        return $user_review_conut;
+    }
 
-        public static function getUserIp($user_id)
-        {
-            $query = Review::where('user_id', '=', $user_id)
+
+    public static function getAvgMark($id)
+    {
+        $query = Review::where('user_id', '=', $id)->get();
+
+        $avg_mark = round($query->avg('mark'), 2);
+
+        return $avg_mark;
+    }
+
+    public static function getUserIp($user_id)
+    {
+        $query = Review::where('user_id', '=', $user_id)
             ->latest()
             ->first();
-            $ip = '0.0.0.0';
-            return $ip;
-        }
+        $ip = '0.0.0.0';
+        return $ip;
+    }
 
-        public static function checkGigabyteUser($login, $password)
-        {
-            $checkUser = Http::asForm()->post(env('CHECK_LINK'), [
-                'user' => $login,
-                'password' => $password,
-            ]);
-            if ($checkUser == '1')
-            {
+    public static function checkGigabyteUser($login, $password)
+    {
+        $checkUser = Http::asForm()->post(env('CHECK_LINK'), [
+            'user' => $login,
+            'password' => $password,
+        ]);
+        if ($checkUser == '1') {
             return true;
-            }
         }
+    }
 }
